a3660b1bb311220e67cb6a38e43dcc3159479a6c
jinredhub <jin@nieblo.com>
2019-06-26 21:47:36 -0700

    fixed mobile nav

--- header.php
+++ header.php
@@ -24,6 +24,7 @@
         position: absolute;
         width: 100%;
         top: 100%;
+        background-color: rgba(60, 57, 57, 1);
     }
     .mobileNavContainer a{
         color: white;
@@ -31,14 +32,14 @@
         text-decoration: none;
         font-size: 17px;
         display: block;
-        background-color: #fcfbfa;
+        /* background-color: #fcfbfa; */
     }
     .mobileNavContainer .hamburgerIcon{
         background-color: transparent;
         display: block;
     }
     .mobileNavContainer a:hover{
-        background-color: #e0e0e0;
+        background-color: #4c4c4c;
     }
 
     /* both============================================ */
