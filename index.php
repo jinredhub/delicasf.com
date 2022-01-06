a31bdc87c5de0e3a509b6c22b4daab1bfad159fd
jinredhub <jin@nieblo.com>
2022-01-05 21:48:11 -0800

    updates hero text

--- index.php
+++ index.php
@@ -35,9 +35,9 @@
                 <span class='titleContainer'>
                     <h1>DELICA</h1>
                     <p>JAPANESE DELI</p>
-                    <p style='background-color: rgba(0,0,0,0.6); border-radius: 16px; border: 1px solid #00dcff; padding: 0 6px; max-width: 400px; margin-top: 14px;'>
+                    <!-- <p style='background-color: rgba(0,0,0,0.6); border-radius: 16px; border: 1px solid #00dcff; padding: 0 6px; max-width: 400px; margin-top: 14px;'>
                         We are closed today<br>1/3, Monday
-                    </p>
+                    </p> -->
                     <a class='btn doordashButton' style='border-radius: 4px; margin-top: 25px;' href="https://order.online/business/delica-218107">
                         <span style='font-size: 16px; background-color: transparent; color: white;'>Order online with</span> <strong>DOORDASH</strong>
                     </a>
